c8ea456b1f9d384df2c875d2e9c80d6baaa32e15
Mithun Kumar S R <mithunkumar.sr@uber.com>
2021-05-22 07:51:14 +0530

    Update testBigram.py

--- testBigram.py
+++ testBigram.py
@@ -1,6 +1,6 @@
 import nltk
 
-word_data = "Mithun is the biggest intellect in the world"
+word_data = "INDIA is the 7th largest country in the world"
 nltk_tokens = nltk.word_tokenize(word_data)
 
 print(list(nltk.bigrams(nltk_tokens)))
